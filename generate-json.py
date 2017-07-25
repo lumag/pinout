805f53dd0ce45ed625870bfa3e29b561b6fb5dcf
radhikap18 <radhikaparalkar814@gmail.com>
2017-07-25 14:08:36 -0400

    Update generate-json.py

--- generate-json.py
+++ generate-json.py
@@ -64,29 +64,29 @@ def load_overlay(overlay):
 
     if 'pincount' in loaded:
         pincount = int(loaded['pincount'])
-        if pincount == 40:
-            details.append('* HAT form-factor')
-        elif pincount == 38:
-            details.append('* pHAT form-factor')
-        elif pincount == 26:
-            details.append('* Classic 26-pin')
-        else:
+        #if pincount == 40:
+        #    details.append('* HAT form-factor')
+        #elif pincount == 38:
+        #    details.append('* pHAT form-factor')
+        #elif pincount == 26:
+        #    details.append('* Classic 26-pin')
+        #else:
             details.append('* {} pin header'.format(pincount))
 
     if 'pin' in loaded:
         uses_5v = False
-        uses_3v = False
+        uses_1v8 = False
         uses = 0
         for pin in loaded['pin']:
             pin = str(pin)
-            if pin.startswith('bcm'):
-                pin = pinout.bcm_to_physical(pin[3:])
+            #if pin.startswith('bcm'):
+            #    pin = pinout.bcm_to_physical(pin[3:])
 
             if pin in pinout.pins:
                 actual_pin = pinout.pins[pin]
 
-                if actual_pin['type'] in ['+3v3', '+5v', 'GND']:
-                    if actual_pin['type'] == '+3v3':
+                #if actual_pin['type'] in ['+3v3', '+5v', 'GND']:
+                    if actual_pin['type'] == '+1v8':
                         uses_3v = True
                     if actual_pin['type'] == '+5v':
                         uses_5v = True
@@ -96,12 +96,12 @@ def load_overlay(overlay):
         if uses > 0:
             details.append('* Uses {} GPIO pins'.format(uses))
 
-        if '3' in loaded['pin'] and '5' in loaded['pin']:
-            pin_3 = loaded['pin']['3']
-            pin_5 = loaded['pin']['5']
-            if 'mode' in pin_3 and 'mode' in pin_5:
-                if pin_3['mode'] == 'i2c' and pin_5['mode'] == 'i2c':
-                    details.append('* Uses I2C')
+        #if '3' in loaded['pin'] and '5' in loaded['pin']:
+        #    pin_3 = loaded['pin']['3']
+        #    pin_5 = loaded['pin']['5']
+        #    if 'mode' in pin_3 and 'mode' in pin_5:
+        #        if pin_3['mode'] == 'i2c' and pin_5['mode'] == 'i2c':
+        #            details.append('* Uses I2C')
 
     if 'url' in loaded:
         details.append('* [More Information]({url})'.format(url=loaded['url']))
