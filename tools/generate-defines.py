ee9f75427bd923d6c13d1cdc99e53ef4cf94e970
radhikap18 <radhikaparalkar814@gmail.com>
2017-07-25 17:17:56 -0400

    Update generate-defines.py

--- tools/generate-defines.py
+++ tools/generate-defines.py
@@ -52,42 +52,42 @@ def slugify(value):
     return re.sub('[-\s]+', '_', value)
 
 
-def bcm_to_physical(pin):
-    pin = pin[3:]
-    for idx in pins:
-        compare_pin = pins[idx]
-        if 'scheme' in compare_pin:
-            if 'bcm' in compare_pin['scheme']:
-                if compare_pin['scheme']['bcm'] == int(pin):
-                    print("Mapping BCM{} to {}".format(pin, str(idx)))
-                    return str(idx)
+#def bcm_to_physical(pin):
+#    pin = pin[3:]
+#    for idx in pins:
+#        compare_pin = pins[idx]
+        #if 'scheme' in compare_pin:
+        #    if 'bcm' in compare_pin['scheme']:
+        #        if compare_pin['scheme']['bcm'] == int(pin):
+        #            print("Mapping BCM{} to {}".format(pin, str(idx)))
+        #            return str(idx)
 
 
-def physical_to_bcm(pin):
-    pin = pins[pin]
-    if 'scheme' in pin:
-        if 'bcm' in pin['scheme']:
-            return str(pin['scheme']['bcm'])
-    return None
+#def physical_to_bcm(pin):
+#    pin = pins[pin]
+#    if 'scheme' in pin:
+#        if 'bcm' in pin['scheme']:
+#            return str(pin['scheme']['bcm'])
+#    return None
 
 
-def physical_to_wiringpi(pin):
-    pin = pins[pin]
-    if 'scheme' in pin:
-        if 'wiringpi' in pin['scheme']:
-            return str(pin['scheme']['wiringpi'])
-    return None
+#def physical_to_wiringpi(pin):
+#    pin = pins[pin]
+#    if 'scheme' in pin:
+#        if 'wiringpi' in pin['scheme']:
+#            return str(pin['scheme']['wiringpi'])
+#    return None
 
 
-def physical_to(pin, scheme='bcm'):
-    if scheme in ['bcm', 'wiringpi']:
-        pin = pins[pin]
-        if 'scheme' in pin:
-            if scheme in pin['scheme']:
-                return str(pin['scheme'][scheme])
-    elif scheme == 'physical':
-        return pin
-    return None
+#def physical_to(pin, scheme='bcm'):
+#    if scheme in ['bcm', 'wiringpi']:
+#        pin = pins[pin]
+#        if 'scheme' in pin:
+#            if scheme in pin['scheme']:
+#                return str(pin['scheme'][scheme])
+#    elif scheme == 'physical':
+#        return pin
+#    return None
 
 
 def add_define(key, value):
